d6330631433f7fb3bce68c5c75519dba7bcfa0da
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-23 23:49:04 -0500

    Initial structure of About page created

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,16 @@
 import React from 'react';
 
-export default function About()
+export default function About() {
+    return (
+        <section id="about">
+            <div>
+                <div>
+                    <h1>
+                      Hi! My name's Christian Sanchez
+                      <br></br>  
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
+}
