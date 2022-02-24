9dc04c68601e77725e3941de29189c63a0aebbc0
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-23 23:16:10 -0500

    created component files. Added import for component files in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App() {
     return (
