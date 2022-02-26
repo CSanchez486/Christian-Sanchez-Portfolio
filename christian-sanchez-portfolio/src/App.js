effed34ffcfb3c3684eb09b61978956f9b978ae7
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-26 18:15:52 -0500

    defined skills and navbar

--- christian-sanchez-portfolio/src/App.js
+++ christian-sanchez-portfolio/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Skills from "./components/Skills";
 
-export default function App() {
+export default function Navbar() {
     return (
         <main className="text-gray-400 bg-gray-900 body-font">
             <Navbar />
