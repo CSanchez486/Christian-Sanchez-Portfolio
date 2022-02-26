5a9a3ccc773bc483c85d80812f44ea1d5fa0e1d9
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-26 10:38:10 -0500

    created template for projects that will be used in portfolio

---
+++ christian-sanchez-portfolio/src/data.js
@@ -0,0 +1,30 @@
+export counts projects = [
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description: "Placeholder",
+        image:"Placeholder",
+        link: "Placeholder",
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description: "Placeholder",
+        image:"Placeholder",
+        link: "Placeholder",
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description: "Placeholder",
+        image:"Placeholder",
+        link: "Placeholder",
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description: "Placeholder",
+        image:"Placeholder",
+        link: "Placeholder",
+    },
+]
