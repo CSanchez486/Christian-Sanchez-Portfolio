6fe98078722ddf3d4472b0776ff21639f977c703
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-26 10:34:04 -0500

    initial code for projects created

--- christian-sanchez-portfolio/src/components/Projects.js
+++ christian-sanchez-portfolio/src/components/Projects.js
@@ -1,4 +1,22 @@
 import React from 'react';
 import projects from "..data/"
 
-export default function Project()
+export default function Project() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>
+                        Apps I've Built
+                    </h1>
+                    <p>
+                        Need to add paragraph re: apps.
+                    </p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
