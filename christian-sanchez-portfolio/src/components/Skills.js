67e80e7ce76ab56d0c1c41f5ddd054ca07a11101
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-26 11:01:56 -0500

    created initials skills template

--- christian-sanchez-portfolio/src/components/Skills.js
+++ christian-sanchez-portfolio/src/components/Skills.js
@@ -1,3 +1,21 @@
 import React from 'react';
 
-// export default function Skills()
+export default function Skills() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>
+                        Skills & Technologies
+                    </h1>
+                    <p>
+                        placeholder for skills & tech
+                    </p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
