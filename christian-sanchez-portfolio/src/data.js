d679d76af36382dcc3223876263cf6fc7c44c3c3
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-28 00:20:55 -0500

    Added project details to data.js file

--- christian-sanchez-portfolio/src/data.js
+++ christian-sanchez-portfolio/src/data.js
@@ -1,30 +1,32 @@
-export const projects = [
+export default projects
+
+const projects = [
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description: "Placeholder",
-        image:"Placeholder",
-        link: "Placeholder",
+        title: "NuMu - Music Finder",
+        subtitle: "NuMu - New Music",
+        description: "Discover new music without being boxed in by algorithms.",
+        image:"https://user-images.githubusercontent.com/94878342/148709887-2566538b-da20-45dc-a2bd-117993e36022.png",
+        link: "https://github.com/CSanchez486/YouTube_Music_Finder",
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description: "Placeholder",
+        title: "Virtual Pet",
+        subtitle: "Telehealth For Pets",
+        description: "Virtual Pets is a web-based app that allows for pet parents to make telehealth consultation appointments with Veterinarians. This allows for a cost-effective way to get professional advice without having to stress your pet with having to go to the vet's office - creating a simple adn stress free solution. ",
         image:"Placeholder",
-        link: "Placeholder",
+        link: "https://github.com/CSanchez486/Virtual_Pet",
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description: "Placeholder",
+        title: "GrandConnect",
+        subtitle: "Video Chat App",
+        description: "GrandConnect was created to connect elementary school children and senior citizens in nursing homes to create a community of empowerment and communication.",
         image:"Placeholder",
-        link: "Placeholder",
+        link: "https://github.com/caitlinw29/GrandConnect",
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description: "Placeholder",
+        title: "Edit That Text",
+        subtitle: "Text Editor",
+        description: "Text editor that edits text.",
         image:"Placeholder",
-        link: "Placeholder",
+        link: "https://github.com/CSanchez486/edit-that-text",
     },
-]
+];
