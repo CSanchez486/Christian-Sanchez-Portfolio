fefa661093d1d716c378ab6000750bf8ac75dd29
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-28 11:49:46 -0500

    added styling to each setion of navbar

--- christian-sanchez-portfolio/src/components/Navbar.js
+++ christian-sanchez-portfolio/src/components/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar() {
                         Christian L. Sanchez
                     </a>
                 </a>
-                <nav>
-                    <a href="#projects">
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                    <a href="#projects" className="mr-5 hover:text-white">
                         Projects
                     </a>
-                    <a href="skills">
+                    <a href="skills" className="mr-5 hover:text-white">
                         Skills
                     </a>
-                    <a href="Contact">
+                    <a href="Contact" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
                         Contact Me
                         <ArrowRightIcon className="w-4 h-4 ml-1" />
                     </a>
