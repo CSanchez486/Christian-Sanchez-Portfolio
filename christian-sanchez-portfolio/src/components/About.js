7ee73f2de65efe8a05bc0050c73ff849b4c8b3aa
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-28 11:57:19 -0500

    added stling to about.js div section

--- christian-sanchez-portfolio/src/components/About.js
+++ christian-sanchez-portfolio/src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function About() {
     return (
         <section id="about">
-            <div>
-                <div>
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1>Hey There!</h1>
                     <h2> My name is Christian Sanchez</h2>
                     <h3> Tech Enthusiast | Something Witty Here </h3>                
