29dd0f71ca512337856e01db7cc53e13f78ef906
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-28 00:33:56 -0500

    added differenr sections to nabvar.

--- christian-sanchez-portfolio/src/components/Navbar.js
+++ christian-sanchez-portfolio/src/components/Navbar.js
@@ -1,3 +1,29 @@
+import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from 'react';
 
-// export default function Navbar()
+export default function Navbar() {
+    return (
+        <header>
+            <div>
+                <a>
+                    <a href="#about">
+                        Christian L. Sanchez
+                    </a>
+                </a>
+                <nav>
+                    <a href="#projects">
+                        Projects
+                    </a>
+                    <a href="skills">
+                        Skills
+                    </a>
+                    <a href="Contact">
+                        Contact Me
+                        <ArrowRightIcon className="w-4 h-4 ml-1" />
+                    </a>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
